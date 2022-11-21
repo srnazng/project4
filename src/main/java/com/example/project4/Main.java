9d8cbf4913ecf6b09895751c9610e7de9be66ab5
srnazng <srnazng@gmail.com>
2022-11-21 15:54:45 -0500

    Main

--- src/main/java/com/example/project4/Main.java
+++ src/main/java/com/example/project4/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  * Launch point of application
  * @author Serena Zeng, Jackson Lee
  */
-public class PizzeriaApplication extends Application {
+public class Main extends Application {
     /**
      * Sets up stage when program starts and load main view
      * @param stage         top level container for program
@@ -19,7 +19,7 @@ public class PizzeriaApplication extends Application {
      */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PizzeriaApplication.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("RU Pizzeria");
         stage.setScene(scene);
